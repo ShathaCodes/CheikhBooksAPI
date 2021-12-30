312716b06ac84cb8154a67b27db3388c6318b824
shatha siala <shatha.siala@gmail.com>
2021-12-30 14:17:37 +0100

    Added mysql database and .env variables, Morgan logger and Helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+    {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import * as helmet from 'helmet';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  dotenv.config();
+  console.log(process.env.DB_USER);
+  app.use(morgan('dev'));
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
